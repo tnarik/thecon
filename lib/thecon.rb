9439358500e16f0dd785b226f1444bb7d08d944e
tnarik <tnarik@gmail.com>
2013-12-04 08:14:34 +0000

    Bumped to version 0.0.2 and further cleanup to release directly from the local repository.

--- lib/thecon.rb
+++ lib/thecon.rb
@@ -5,13 +5,13 @@ require 'socket'
 
 module Thecon
   # Your code goes here...
-  def self.ready?(port=80, ip = "localhost", connection_timeout = 6, read_timeout = 2)
+  def self.ready?(port=80, ip = "localhost", connection_timeout = 3, read_timeout = 2)
   	result = false
 		begin
   		timeout( connection_timeout ) do
         socket = Socket.new :INET, :STREAM
-        a = Socket.pack_sockaddr_in port, ip
-        socket.connect a
+        sockaddr = Socket.pack_sockaddr_in port, ip
+        socket.connect sockaddr
         begin
           timeout( read_timeout ) do
             r,w,e = select([socket], nil, nil, 1) # nil, nil, nil from inside the network
@@ -22,42 +22,22 @@ module Thecon
               p "not available for read: either doesn't exist or not closed" #TRUE
               result = true
             end
-
-            if ( socket.read == "") then
-              p "closed " #closed externally
-            else
-              result = true
-            end
-          end
         rescue StandardError => ka
-          p "OK, standardERROR"
-          p ka
+          p "OK, standardERROR #{ka}" #If socket.read there could be errors (this shouldn't be triggered)
           result = true
         rescue Timeout::Error => e
-          p "timedout reading" #Socket is there but there are not data
+          p "timedout reading" #Socket is there but there are not data to read (if socket.read)
           result = true
         end
-
-        # rescue Timeout::Error , StandardError => e#Exception => e
-        #  #if (e.message == "execution expired")
-        #  #  p "received? #{e.exception}"    # When reading from a server that doesn't send, which is quite normal
-        #  #else
-        #  #  raise e
-        #  #end
-        #  p "goatchas"
-        #end
-        
       end
     rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-      p "connection refused" # Connection expires on its own
-      result=false
+      result=false # Connection fails on its own
     rescue Timeout::Error, StandardError => e
-      # This will happen when connection is imposible during the timeout period
-      p "connection:  #{e}"   # When host or port is wrong   #THIS means connection timesout
-      result=false
+      result=false # When host or port is wrong (connection timesout or some other Errno)
     rescue
     end
-    return result
+
+    result
   end
 
 end
