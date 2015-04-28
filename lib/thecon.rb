92a344994de8c05ffdc73e5702368e5c57346a33
tnarik <tnarik@lecafeautomatique.co.uk>
2015-04-28 15:26:44 +0100

    Alternative implementation based on non-blocking reading. Logic is still a bit confussing because in DUS I experienced a 'positive' (connection detected) while in a WiFi (with password) environment. But I cannot test the same conditions now.

--- lib/thecon.rb
+++ lib/thecon.rb
@@ -10,6 +10,7 @@ module Thecon
   def self.ready?(port=80, ip = "localhost", connection_timeout = 3, read_timeout = 2)
     @log = Logger.new(STDOUT)
 
+    port ||= 80
     result = false
     begin
       timeout( connection_timeout ) do
@@ -18,16 +19,32 @@ module Thecon
         socket.connect sockaddr
         begin
           timeout( read_timeout ) do
+            begin
+
+            response = socket.read_nonblock(1)
+
             r,w,e = select([socket], nil, nil, 1) # nil, nil, nil from inside the network
-            unless r.nil? then
-              @log.debug "socket was closed" if ( r[0].read(1).nil? ) #This means there is something there
-              result = false #Something closed this
-            else
-              @log.debug "not available for read: either doesn't exist or not closed" #TRUE
-              result = true
+            result = true
+            rescue IO::WaitReadable
+              @log.debug "Wait readable"
+              r,w,e = select([socket], nil, nil, 1) # nil, nil, nil from inside the network
+              unless r.nil? then
+                if r[0].read(1).nil?
+                  @log.debug "Readable socket was closed / there is NOTHING there"
+                  result = false
+                else
+                  @log.debug "There is something there" 
+                  result = true
+                end
+              else
+                @log.debug "not available for read: either doesn't exist or not closed" #TRUE
+                result = true
+              end
+            rescue IO::WaitWritable
             end
+
           end
-        rescue StandardError => ka
+        rescue StandardError => ka #This can get triggered i
           @log.debug "OK, standardERROR #{ka}" #If socket.read there could be errors (this shouldn't be triggered)
           result = true
         rescue Timeout::Error => e
